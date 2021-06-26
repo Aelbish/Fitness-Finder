57fff1204673d5993ecc58c9cfc9eabe7b6f910a
Aelbish Shrestha <aelbish@gmail.com>
2021-06-26 02:14:23 -0500

    Added basic styling with bootstrap v5. Added sort by category for workout plans.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
 const WorkoutPlan = require("./models/workoutPlan");
 
@@ -19,6 +20,9 @@ db.once("open", () => {
 });
 
 const app = express();
+
+//for including boilerplate
+app.engine("ejs", ejsMate);
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
